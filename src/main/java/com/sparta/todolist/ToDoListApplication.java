c846c2dd0cbeb91a6097d8db5ab668e68bda914a
guncow0603 <guncow0603@gmail.com>
2023-11-15 17:52:49 +0900

    회원가입 구현

--- src/main/java/com/sparta/todolist/ToDoListApplication.java
+++ src/main/java/com/sparta/todolist/ToDoListApplication.java
@@ -1,9 +1,9 @@
 package com.sparta.todolist;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class) // Spring Security 인증 기능 제외
 public class ToDoListApplication {
 
 	public static void main(String[] args) {
